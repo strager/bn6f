6c04daf90af10b28a032631b975bfec6d180abf2
Matthew Glazar <strager.nds@gmail.com>
2020-06-27 17:03:09 -0700

    yay

--- map2png.py
+++ map2png.py
@@ -9,7 +9,6 @@ gbagfx_exe = pathlib.Path(__file__).parent / "tools" / "gbagfx" / "gbagfx"
 def main():
     with tempfile.TemporaryDirectory() as temporary_directory:
         temp_dir = pathlib.Path(temporary_directory)
-        tile_set = PIL.Image.open("acdc-tiles.png")
         with open("bn6f.gba", "rb") as rom_file:
             rom_file.seek(0x4ff4ac)
             map_header = rom_file.read(12)
@@ -17,8 +16,31 @@ def main():
             map_compressed_data = rom_file.read(map_compressed_size)
             map_data = decompress(map_compressed_data)
 
-        #map_data = pathlib.Path("acdc.map").read_bytes()
-        create_image_for_map(tile_set=tile_set, map_data=map_data).save("acdc.png")
+            rom_file.seek(0x4ff308+4)
+            palette = rom_file.read(256 * 2)
+
+            rom_file.seek(0x4fafcc)
+            tile_set_header = rom_file.read(24)
+            tile_set_compressed_size = 0x84ff4ac-0x4fafcc # @@@
+            tile_set_compressed_data = rom_file.read(tile_set_compressed_size)
+            tile_set_data = decompress(tile_set_compressed_data)
+
+            tile_set_png_path = temp_dir / "tile-set.png"
+            tile_set_data_path = temp_dir / "tile-set.8bpp"
+            tile_set_data_path.write_bytes(tile_set_data)
+            palette_path = temp_dir / "tile-set.pal"
+            palette_path.write_bytes(palette)
+            subprocess.check_call([
+                gbagfx_exe,
+                str(tile_set_data_path),
+                str(tile_set_png_path),
+                "-palette",
+                str(palette_path),
+                "-width", "16",
+            ])
+
+            tile_set = PIL.Image.open(tile_set_png_path)
+            create_image_for_map(tile_set=tile_set, map_data=map_data).save("acdc.png")
 
 def decompress(compressed_data: bytes) -> bytes:
     with tempfile.TemporaryDirectory() as temporary_directory:
