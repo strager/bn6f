d3adf4969103defcbdeeb8c0a61fbf496d9a42b8
Matthew Glazar <strager.nds@gmail.com>
2020-06-27 22:13:22 -0700

    tweak

--- sprite2png.py
+++ sprite2png.py
@@ -33,72 +33,85 @@ def main():
                 data = rom_file.read(size)
                 return struct.unpack(format, data)
 
-            sprite_address = 0x081d8000
-            (tiles_in_biggest_tileset, _constant_0, _constant_1, animation_count) = unpack_at_offset(address_to_rom_offset(sprite_address), "<BBBB")
+            for sprite_pointer_address in range(0x08031cec, 0x80329a8, 4):
+                (sprite_address,) = unpack_at_offset(address_to_rom_offset(sprite_pointer_address), "<I")
+                if sprite_address == 0x00000000:
+                    print(f"!!! sprite_address={sprite_address:#08x}")
+                    continue
 
-            animation_table_rom_offset = rom_file.tell()
-            # _animoffset variables hold pointers relative to animation_table_rom_offset.
-            (animation_animoffset,) = unpack_at_offset(animation_table_rom_offset, "<I")
-            (tile_set_animoffset, palettes_animoffset, mini_animations_animoffset, object_lists_animoffset, delay, _constant_0, flags, _constant_0) = unpack_at_offset(animation_table_rom_offset + animation_animoffset, "<IIIIBBBB")
+                (tiles_in_biggest_tileset, _constant_0, _constant_1, animation_count) = unpack_at_offset(address_to_rom_offset(sprite_address & 0x7fffffff), "<BBBB")
 
-            (tile_set_size,) = unpack_at_offset(animation_table_rom_offset + tile_set_animoffset, "<I")
-            tile_set_4bpp = rom_file.read(tile_set_size)
+                animation_table_rom_offset = rom_file.tell()
+                # _animoffset variables hold pointers relative to animation_table_rom_offset.
+                (animation_animoffset,) = unpack_at_offset(animation_table_rom_offset, "<I")
+                if animation_animoffset > 0x1000:
+                    # probably not a valid sprite.
+                    print(f"!!! animation_table_rom_offset={animation_table_rom_offset:#08x} animation_animoffset={animation_animoffset:#x}")
+                    continue
+                (tile_set_animoffset, palettes_animoffset, mini_animations_animoffset, object_lists_animoffset, delay, _constant_0, flags, _constant_0) = unpack_at_offset(animation_table_rom_offset + animation_animoffset, "<IIIIBBBB")
+                if tile_set_animoffset > 0x1000:
+                    # probably not a valid sprite.
+                    print(f"!!! tile_set_animoffset={tile_set_animoffset:#x}")
+                    continue
 
-            (palette_size,) = unpack_at_offset(animation_table_rom_offset + palettes_animoffset, "<I")
-            palette = rom_file.read(palette_size)
+                (tile_set_size,) = unpack_at_offset(animation_table_rom_offset + tile_set_animoffset, "<I")
+                tile_set_4bpp = rom_file.read(tile_set_size)
+
+                (palette_size,) = unpack_at_offset(animation_table_rom_offset + palettes_animoffset, "<I")
+                palette = rom_file.read(palette_size)
 
 
-            tile_set_png_path = temp_dir / "tile-set.png"
-            tile_set_data_path = temp_dir / "tile-set.4bpp"
-            tile_set_data_path.write_bytes(tile_set_4bpp)
-            palette_path = temp_dir / "tile-set.pal"
-            palette_path.write_bytes(palette)
-            subprocess.check_call([
-                gbagfx_exe,
-                str(tile_set_data_path),
-                str(tile_set_png_path),
-                "-palette",
-                str(palette_path),
-                "-width", "1",
-            ])
+                tile_set_png_path = temp_dir / "tile-set.png"
+                tile_set_data_path = temp_dir / "tile-set.4bpp"
+                tile_set_data_path.write_bytes(tile_set_4bpp)
+                palette_path = temp_dir / "tile-set.pal"
+                palette_path.write_bytes(palette)
+                subprocess.check_call([
+                    gbagfx_exe,
+                    str(tile_set_data_path),
+                    str(tile_set_png_path),
+                    "-palette",
+                    str(palette_path),
+                    "-width", "1",
+                ])
 
-            temp_tile_set_png_path = temp_dir / "temp-tile-set.png"
-            temp_tile_set = PIL.Image.open(tile_set_png_path)
-            temp_tile_set.save(temp_tile_set_png_path, transparency=0)
-            tile_set = PIL.Image.open(temp_tile_set_png_path).convert("RGBA")
+                temp_tile_set_png_path = temp_dir / "temp-tile-set.png"
+                temp_tile_set = PIL.Image.open(tile_set_png_path)
+                temp_tile_set.save(temp_tile_set_png_path, transparency=0)
+                tile_set = PIL.Image.open(temp_tile_set_png_path).convert("RGBA")
 
-            object_list_rom_offset = animation_table_rom_offset + object_lists_animoffset
-            # _objoffset variables hold pointers relative to object_list_rom_offset.
-            (object_list_objoffset,) = unpack_at_offset(object_list_rom_offset, "<I")
-            rom_file.seek(object_list_rom_offset + object_list_objoffset)
+                object_list_rom_offset = animation_table_rom_offset + object_lists_animoffset
+                # _objoffset variables hold pointers relative to object_list_rom_offset.
+                (object_list_objoffset,) = unpack_at_offset(object_list_rom_offset, "<I")
+                rom_file.seek(object_list_rom_offset + object_list_objoffset)
 
-            object_list = []
-            while True:
-                (tile_index, x, y, flags) = unpack("<BbbH")
-                if tile_index == 0xff:
-                    break
-                object_list.append(ObjectListEntry(
-                    tile_index=tile_index,
-                    x=x,
-                    y=y,
-                    size_flag = (flags >> 0) & 3,
-                    h_flip = (flags & (1<<6)) != 0,
-                    v_flip = (flags & (1<<7)) != 0,
-                    shape_flag = (flags >> 8) & 3,
-                    palette_index = (flags >> 12),
-                ))
+                object_list = []
+                while True:
+                    (tile_index, x, y, flags) = unpack("<BbbH")
+                    if tile_index == 0xff:
+                        break
+                    object_list.append(ObjectListEntry(
+                        tile_index=tile_index,
+                        x=x,
+                        y=y,
+                        size_flag = (flags >> 0) & 3,
+                        h_flip = (flags & (1<<6)) != 0,
+                        v_flip = (flags & (1<<7)) != 0,
+                        shape_flag = (flags >> 8) & 3,
+                        palette_index = (flags >> 12),
+                    ))
 
-            sprite = PIL.Image.new("RGBA", (256, 256))
-            for object_list_entry in object_list:
-                (object_width, object_height) = object_list_entry.size
-                tile_index = object_list_entry.tile_index
-                for yi in range(0, object_height, TILE_HEIGHT):
-                    for xi in range(0, object_width, TILE_WIDTH):
-                        sprite.alpha_composite(tile_set, (object_list_entry.x + xi + 128, object_list_entry.y + yi + 128), (0, tile_index * TILE_HEIGHT, TILE_WIDTH, tile_index * TILE_HEIGHT + TILE_HEIGHT))
-                        tile_index += 1
-            sprite_png_path = pathlib.Path("sprite.png")
-            sprite.save(sprite_png_path)
-            print(f"wrote {sprite_png_path}")
+                sprite = PIL.Image.new("RGBA", (256, 256))
+                for object_list_entry in object_list:
+                    (object_width, object_height) = object_list_entry.size
+                    tile_index = object_list_entry.tile_index
+                    for yi in range(0, object_height, TILE_HEIGHT):
+                        for xi in range(0, object_width, TILE_WIDTH):
+                            sprite.alpha_composite(tile_set, (object_list_entry.x + xi + 128, object_list_entry.y + yi + 128), (0, tile_index * TILE_HEIGHT, TILE_WIDTH, tile_index * TILE_HEIGHT + TILE_HEIGHT))
+                            tile_index += 1
+                sprite_png_path = pathlib.Path(f"sprite-{sprite_address:08x}.png")
+                sprite.save(sprite_png_path)
+                #print(f"wrote {sprite_png_path}")
 
 class ObjectListEntry(typing.NamedTuple):
     tile_index: int
