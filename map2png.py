8e25c8224e4af21780e3c5d9ea0c9bda9f93f253
Matthew Glazar <strager.nds@gmail.com>
2020-06-27 18:04:16 -0700

    sorta fix net palettes

--- map2png.py
+++ map2png.py
@@ -11,55 +11,70 @@ gbagfx_exe = pathlib.Path(__file__).parent / "tools" / "gbagfx" / "gbagfx"
 def address_to_rom_offset(address: int) -> int:
     return address - 0x08000000
 
+def chunk(data: bytes, bytes_per_chunk: int) -> typing.List[bytes]:
+    chunks = []
+    for chunk_start_index in range(0, len(data), bytes_per_chunk):
+        chunks.append(data[chunk_start_index:chunk_start_index+bytes_per_chunk])
+    return chunks
+
 def main():
     with tempfile.TemporaryDirectory() as temporary_directory:
         temp_dir = pathlib.Path(temporary_directory)
         with open("bn6f.gba", "rb") as rom_file:
-            rom_file.seek(address_to_rom_offset(0x8032a20+0xc*4))
-            area_header = rom_file.read(3 * 4)
-            (tile_set_address, palette_address, maps_address) = struct.unpack("<III", area_header)
+            table_start_address = 0x8032a74
+            for area_number in range(3):
+                print(f"=== area #{area_number} in table 0x{table_start_address:08x} ===")
 
-            rom_file.seek(address_to_rom_offset(maps_address))
-            maps_header = rom_file.read(12)
-            (map_width, map_height, _unknown_2, _unknown_3, maps_data_start_offset, maps_compressed_size) = struct.unpack("<BBBBII", maps_header)
-            rom_file.seek(maps_data_start_offset - 12, os.SEEK_CUR)
-            maps_compressed_data = rom_file.read(maps_compressed_size)
-            maps_data = decompress(maps_compressed_data)
-            bottom_layer_maps_data = maps_data[:map_width * map_height * 2]
-            top_layer_maps_data = maps_data[map_width * map_height * 2:]
+                rom_file.seek(address_to_rom_offset(table_start_address + 0xc*area_number))
+                area_header = rom_file.read(3 * 4)
+                (tile_set_address, palettes_address, maps_address) = struct.unpack("<III", area_header)
 
-            rom_file.seek(address_to_rom_offset(palette_address))
-            palette_header = rom_file.read(4)
-            (palette_size,) = struct.unpack("<I", palette_header)
-            palette = rom_file.read(palette_size)
+                rom_file.seek(address_to_rom_offset(maps_address))
+                maps_header = rom_file.read(12)
+                (map_width, map_height, _unknown_2, _unknown_3, maps_data_start_offset, maps_compressed_size) = struct.unpack("<BBBBII", maps_header)
+                print(f"unknown_2: 0x{_unknown_2:02x}")
+                print(f"unknown_3: 0x{_unknown_3:02x}")
+                rom_file.seek(maps_data_start_offset - 12, os.SEEK_CUR)
+                maps_compressed_data = rom_file.read(maps_compressed_size)
+                maps_data = decompress(maps_compressed_data)
+                bottom_layer_maps_data = maps_data[:map_width * map_height * 2]
+                top_layer_maps_data = maps_data[map_width * map_height * 2:]
 
-            rom_file.seek(address_to_rom_offset(tile_set_address))
-            tile_set_header = rom_file.read(20)
-            (_unknown_0, tile_set_data_start_offset, _unknown_8, _unknown_c, tile_set_compressed_size) = struct.unpack("<IIIII", tile_set_header)
-            rom_file.seek(tile_set_data_start_offset - 20, os.SEEK_CUR)
-            tile_set_compressed_data = rom_file.read(tile_set_compressed_size)
-            tile_set_data = decompress(tile_set_compressed_data)
+                rom_file.seek(address_to_rom_offset(palettes_address))
+                palettes_header = rom_file.read(4)
+                (palettes_size,) = struct.unpack("<I", palettes_header)
+                palettes_data = rom_file.read(palettes_size)
+                palettes = chunk(palettes_data, 256*2)
+                palette = palettes[0]
 
-            tile_set_png_path = temp_dir / "tile-set.png"
-            tile_set_data_path = temp_dir / "tile-set.8bpp"
-            tile_set_data_path.write_bytes(tile_set_data)
-            palette_path = temp_dir / "tile-set.pal"
-            palette_path.write_bytes(palette)
-            subprocess.check_call([
-                gbagfx_exe,
-                str(tile_set_data_path),
-                str(tile_set_png_path),
-                "-palette",
-                str(palette_path),
-                "-width", "16",
-            ])
+                rom_file.seek(address_to_rom_offset(tile_set_address))
+                tile_set_header = rom_file.read(20)
+                (_unknown_0, tile_set_data_start_offset, _unknown_8, _unknown_c, tile_set_compressed_size) = struct.unpack("<IIIII", tile_set_header)
+                rom_file.seek(tile_set_data_start_offset - 20, os.SEEK_CUR)
+                tile_set_compressed_data = rom_file.read(tile_set_compressed_size)
+                tile_set_data = decompress(tile_set_compressed_data)
 
-            temp_tile_set_png_path = temp_dir / "temp-tile-set.png"
-            temp_tile_set = PIL.Image.open(tile_set_png_path)
-            temp_tile_set.save(temp_tile_set_png_path, transparency=0)
+                tile_set_png_path = temp_dir / "tile-set.png"
+                tile_set_data_path = temp_dir / "tile-set.8bpp"
+                tile_set_data_path.write_bytes(tile_set_data)
+                palette_path = temp_dir / "tile-set.pal"
+                palette_path.write_bytes(palette)
+                subprocess.check_call([
+                    gbagfx_exe,
+                    str(tile_set_data_path),
+                    str(tile_set_png_path),
+                    "-palette",
+                    str(palette_path),
+                    "-width", "16",
+                ])
 
-            tile_set = PIL.Image.open(temp_tile_set_png_path)
-            create_image_for_map(tile_set=tile_set, layer_map_datas=[bottom_layer_maps_data, top_layer_maps_data], map_width=map_width, map_height=map_height).save("acdc.png")
+                temp_tile_set_png_path = temp_dir / "temp-tile-set.png"
+                temp_tile_set = PIL.Image.open(tile_set_png_path)
+                temp_tile_set.save(temp_tile_set_png_path, transparency=0)
+
+                tile_set = PIL.Image.open(temp_tile_set_png_path)
+                area_image = create_image_for_map(tile_set=tile_set, layer_map_datas=[bottom_layer_maps_data, top_layer_maps_data], map_width=map_width, map_height=map_height)
+                area_image.save(f"area-{table_start_address:08x}-{area_number}.png")
 
 def decompress(compressed_data: bytes) -> bytes:
     with tempfile.TemporaryDirectory() as temporary_directory:
