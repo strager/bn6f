c0ae975aa7b9037589d5b197ddf90095413bb4ce
Matthew Glazar <strager.nds@gmail.com>
2020-06-28 20:34:41 -0700

    draw subsprites

--- util.py
+++ util.py
@@ -47,8 +47,23 @@ def get_overworld_sprite_image(rom: ROM, owsprite_index: int):
     owsprite_table_address = 0x80a3a44
     obj_sprite_pointers_address = 0x08032314
 
-    (sprite_index,) = rom.unpack_at_offset(address_to_rom_offset(owsprite_table_address + owsprite_index*0x10 + 0x1), "<B")
-    print(f"sprite_index={sprite_index:#x}")
+    (
+        _unknown_0,
+        sprite_index,
+        sprite_subindex,
+        _unknown_3,
+        _unknown_4,
+        palette_index,
+        _unknown_9,
+        _unknown_a,
+        _unknown_c,
+        _unknown_e,
+    ) = rom.unpack_at_offset(
+        address_to_rom_offset(owsprite_table_address + owsprite_index*0x10),
+        "<BBBBIBBHHH",
+    )
+    print(f"sprite_index={sprite_index:#x} sprite_subindex={sprite_subindex:#x}")
+    print(( _unknown_0, sprite_index, sprite_subindex, _unknown_3, _unknown_4, palette_index, _unknown_9, _unknown_a, _unknown_c, _unknown_e,))
     (owsprite_address,) = rom.unpack_at_offset(address_to_rom_offset(obj_sprite_pointers_address + sprite_index*0x4), "<I")
     # Does top bit set mean we have to use the overworld
     # sprite format instead of the animation sprite
@@ -57,9 +72,9 @@ def get_overworld_sprite_image(rom: ROM, owsprite_index: int):
     print(f"owsprite_address={owsprite_address:#x}")
     #owsprite_address = 0x84c3c90
     #owsprite_address = 0x84c3c90
-    return get_overworld_sprite_image_raw(rom=rom, owsprite_address=owsprite_address)
+    return get_overworld_sprite_image_raw(rom=rom, owsprite_address=owsprite_address, sprite_subindex=sprite_subindex)
 
-def get_overworld_sprite_image_raw(rom: ROM, owsprite_address: int):
+def get_overworld_sprite_image_raw(rom: ROM, owsprite_address: int, sprite_subindex: int):
     with tempfile.TemporaryDirectory() as temporary_directory:
         temp_dir = pathlib.Path(temporary_directory)
         (compressed_size,) = rom.unpack_at_offset(address_to_rom_offset(owsprite_address), "<I")
@@ -71,57 +86,62 @@ def get_overworld_sprite_image_raw(rom: ROM, owsprite_address: int):
         (_unknown_4, _unknown_5, _unknown_6, sprite_count) = struct.unpack_from("<BBBB", data_with_header, 4)
         if sprite_count == 0:
             raise Exception(f"no sprites found")
+        if sprite_subindex >= sprite_count:
+            raise ValueError(f"sprite index ({sprite_subindex}) out of range (sprite_count={sprite_count})")
         sprite_header_offsets = [
-                struct.unpack_from("<I", data_with_header, 8 + sprite_index*4)[0]
-                for sprite_index in range(sprite_count)
+                struct.unpack_from("<I", data_with_header, 8 + sprite_subindex*4)[0]
+                for sprite_subindex in range(sprite_count)
         ]
         data = data_with_header[8:]
-        for sprite_index in range(sprite_count):
-            sprite_header_offset = sprite_header_offsets[sprite_index]
-            (tile_set_offset, palettes_offset, _unknown_14, object_list_offset, _unknown_1c) = struct.unpack_from("<IIIII", data, sprite_header_offset)
 
-            (palettes_size,) = struct.unpack_from("<I", data, palettes_offset)
-            PALETTE_SIZE = 16*2
-            palette_datas = chunk(
-                    data[palettes_offset+4:palettes_offset+4+palettes_size],
-                    PALETTE_SIZE)
+        sprite_header_offset = sprite_header_offsets[sprite_subindex]
+        (tile_set_offset, palettes_offset, _unknown_14, object_list_offset, _unknown_1c) = struct.unpack_from("<IIIII", data, sprite_header_offset)
 
-            (tile_set_size,) = struct.unpack_from("<I", data, tile_set_offset)
-            print(f"tile_set {tile_set_offset:#x}..{tile_set_offset+tile_set_size:#x}")
-            tile_set_data = data[tile_set_offset+4:tile_set_offset+4+tile_set_size]
+        (palettes_size,) = struct.unpack_from("<I", data, palettes_offset)
+        PALETTE_SIZE = 16*2
+        palette_datas = chunk(
+                data[palettes_offset+4:palettes_offset+4+palettes_size],
+                PALETTE_SIZE)
 
-            tile_sets = [
-                make_sprite_tile_set_image(tile_set_data=tile_set_data, palette_data=palette_data)
-                for palette_data in palette_datas
-            ]
+        (tile_set_size,) = struct.unpack_from("<I", data, tile_set_offset)
+        print(f"tile_set {tile_set_offset:#x}..{tile_set_offset+tile_set_size:#x}")
+        tile_set_data = data[tile_set_offset+4:tile_set_offset+4+tile_set_size]
 
-            object_list = []
-            while True:
-                (tile_index, x, y, flags) = struct.unpack_from("<BbbH", data, object_list_offset + 4)
-                if tile_index == 0xff:
-                    break
-                object_list.append(ObjectListEntry(
-                    tile_index=tile_index,
-                    x=x,
-                    y=y,
-                    size_flag = (flags >> 0) & 3,
-                    h_flip = (flags & (1<<6)) != 0,
-                    v_flip = (flags & (1<<7)) != 0,
-                    shape_flag = (flags >> 8) & 3,
-                    palette_index = (flags >> 12),
-                ))
-                object_list_offset += 5
+        tile_sets = [
+            make_sprite_tile_set_image(tile_set_data=tile_set_data, palette_data=palette_data)
+            for palette_data in palette_datas
+        ]
 
-            sprite = PIL.Image.new("RGBA", (256, 256))
-            for object_list_entry in object_list:
-                (object_width, object_height) = object_list_entry.size
-                tile_index = object_list_entry.tile_index
-                for yi in range(0, object_height, TILE_HEIGHT):
-                    for xi in range(0, object_width, TILE_WIDTH):
-                        sprite.alpha_composite(tile_sets[object_list_entry.palette_index], (object_list_entry.x + xi + 128, object_list_entry.y + yi + 128), (0, tile_index * TILE_HEIGHT, TILE_WIDTH, tile_index * TILE_HEIGHT + TILE_HEIGHT))
-                        tile_index += 1
-            # @@@ todo: compose sprites?
-            return sprite
+        object_list = []
+        while True:
+            (tile_index, x, y, flags) = struct.unpack_from("<BbbH", data, object_list_offset + 4)
+            if tile_index == 0xff:
+                break
+            object_list.append(ObjectListEntry(
+                tile_index=tile_index,
+                x=x,
+                y=y,
+                size_flag = (flags >> 0) & 3,
+                h_flip = (flags & (1<<6)) != 0,
+                v_flip = (flags & (1<<7)) != 0,
+                shape_flag = (flags >> 8) & 3,
+                palette_index = (flags >> 12),
+            ))
+            object_list_offset += 5
+
+        sprite = PIL.Image.new("RGBA", (256, 256))
+        for object_list_entry in object_list:
+            (object_width, object_height) = object_list_entry.size
+            tile_index = object_list_entry.tile_index
+            for yi in range(0, object_height, TILE_HEIGHT):
+                for xi in range(0, object_width, TILE_WIDTH):
+                    if object_list_entry.palette_index >= len(tile_sets):
+                        tile_set = tile_sets[0] # HACK
+                    else:
+                        tile_set = tile_sets[object_list_entry.palette_index]
+                    sprite.alpha_composite(tile_set, (object_list_entry.x + xi + 128, object_list_entry.y + yi + 128), (0, tile_index * TILE_HEIGHT, TILE_WIDTH, tile_index * TILE_HEIGHT + TILE_HEIGHT))
+                    tile_index += 1
+        return sprite
 
 def make_sprite_tile_set_image(tile_set_data: bytes, palette_data: bytes):
     with tempfile.TemporaryDirectory() as temporary_directory:
