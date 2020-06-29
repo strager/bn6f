149b7c869b43acdd44acac8e714b0623c78837ea
Matthew Glazar <strager.nds@gmail.com>
2020-06-29 16:27:59 -0700

    Label CentralTown map object spawns
    Create map_object_spawn_data_struct to document the fields of
    CentralTown's object lists.

--- maps/CentralTown/loader.s
+++ maps/CentralTown/loader.s
@@ -97,10 +97,10 @@ CentralTown_SpawnMapObjectsForMap:
 	.pool // off_804E734
 off_804E738:
 	// <endpool>
-	.word byte_804E74C
-	.word byte_804E87C
-	.word dword_804E8D0
-	.word dword_804E8D4
-	.word byte_804E8D8
+	.word CentralTown_Map0_ObjectSpawns
+	.word CentralTown_Map1_ObjectSpawns
+	.word CentralTown_Map2_ObjectSpawns
+	.word CentralTown_Map3_ObjectSpawns
+	.word CentralTown_Map4_ObjectSpawns
 	thumb_func_end CentralTown_SpawnMapObjectsForMap
 /*For debugging purposes, connect comment at any range!*/
